ddf31b1170ea590cb9e2fea8f92e1976b71be1de
nrasadi <navidreza.asadi@gmail.com>
2021-01-04 01:35:20 +0330

    Complete contact section; Add profile picture

--- scripts/script.js
+++ scripts/script.js
@@ -31,10 +31,12 @@ function windowSizeChanged() {
   // alert(window.innerWidth);
   var mainContent = document.getElementById("main-content");
   var mainnav = document.getElementById("main-topnav");
+  var burger = document.getElementById("burger");
   if (window.innerWidth >= 650){
     mainnav.className = "topnav";
+    burger.className = '';
   }
-  if (!mainnav.className.includes("responsive")){
+  else if (!mainnav.className.includes("responsive")){
     mainContent.style.marginTop = '80px';
   }
 }
@@ -58,3 +60,79 @@ $(document).ready(function(){
 		$(this).toggleClass('open');
 	});
 });
+
+
+// document.getElementsByClassName("tablink")[0].click();
+
+function openContactTab(evt, tabName) {
+  var i, x, tablinks;
+  x = document.getElementsByClassName("contactTab");
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";
+  }
+  // tablinks = document.getElementsByClassName("tablink");
+  // console.log(tablinks);
+  // for (i = 0; i < x.length; i++) {
+  //   tablinks[i].classList.remove("w3-green");
+  // }
+  document.getElementById(tabName).style.display = "block";
+  // evt.currentTarget.classList.add("w3-green");
+}
+
+// var modal = document.getElementById('contact-modal');
+// // When the user clicks anywhere outside of the modal, close it
+// window.onclick = function(event) {
+//   var kevent = event.target
+//   console.log(event.target);
+//   // console.log("SALAM");
+//   if (event.target == modal) {
+//     alert("HEY")
+//     modal.style.display = "none";
+//   }
+// }
+
+window.addEventListener("DOMContentLoaded", function() {
+
+  // get the form elements defined in your form HTML above
+
+  var form = document.getElementById("my-form");
+  var button = document.getElementById("my-form-button");
+  var status = document.getElementById("my-form-status");
+
+  // Success and Error functions for after the form is submitted
+
+  function success() {
+    form.reset();
+    button.style = "display: none ";
+    status.innerHTML = "Your message has been sent. Thank you!";
+  }
+
+  function error() {
+    status.innerHTML = "Sorry! There was a problem with sending your message. Please try again later.";
+  }
+
+  // handle the form submission event
+
+  form.addEventListener("submit", function(ev) {
+    ev.preventDefault();
+    var data = new FormData(form);
+    ajax(form.method, form.action, data, success, error);
+  });
+});
+
+// helper function for sending an AJAX request
+
+function ajax(method, url, data, success, error) {
+  var xhr = new XMLHttpRequest();
+  xhr.open(method, url);
+  xhr.setRequestHeader("Accept", "application/json");
+  xhr.onreadystatechange = function() {
+    if (xhr.readyState !== XMLHttpRequest.DONE) return;
+    if (xhr.status === 200) {
+      success(xhr.response, xhr.responseType);
+    } else {
+      error(xhr.status, xhr.response, xhr.responseType);
+    }
+  };
+  xhr.send(data);
+}
